4cf20a7f7e7d1f710bd323433b65734e6942e403
PrajjwalLyzr <prajjwal@lyzr.ai>
2024-05-03 21:17:13 +0530

    update app to extract all the long video content

--- yt_scrapper.py
+++ yt_scrapper.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from googleapiclient.discovery import build
+import isodate
 import pandas as pd
 import os
 from dotenv import load_dotenv; load_dotenv()
@@ -24,15 +25,67 @@ def get_videos_ids(youtube, playlist_id):
 
     request = youtube.playlistItems().list(
         part='contentDetails',
-        playlistId = playlist_id,
-        maxResults = 50)
-    
+        playlistId=playlist_id,
+        maxResults=50)
+
     response = request.execute()
 
     video_ids = []
 
     for i in range(len(response['items'])):
-        video_ids.append(response['items'][i]['contentDetails']['videoId'])
+        video_id = response['items'][i]['contentDetails']['videoId']
+        
+
+        video_request = youtube.videos().list(
+            part='contentDetails',
+            id=video_id
+        )
+        video_response = video_request.execute()
+        duration = video_response['items'][0]['contentDetails']['duration']
+        
+
+        # Convert ISO 8601 duration to seconds
+        duration_seconds = isodate.parse_duration(duration).total_seconds()
+        
+        # Exclude videos with duration less than 1 minutes (60 seconds)
+        if duration_seconds >= 60:
+            video_ids.append(video_id)
+    
+    next_page_token = response.get('nextPageToken')
+    more_pages = True
+
+    while more_pages:
+        if next_page_token is None:
+            more_pages = False
+        else:
+            request  =  youtube.playlistItems().list(
+                        part='contentDetails',
+                        playlistId=playlist_id,
+                        maxResults=50,
+                        pageToken = next_page_token)
+            
+            response = request.execute()
+
+            for i in range(len(response['items'])):
+                video_id = response['items'][i]['contentDetails']['videoId']
+                
+
+                video_request = youtube.videos().list(
+                    part='contentDetails',
+                    id=video_id
+                )
+                video_response = video_request.execute()
+                duration = video_response['items'][0]['contentDetails']['duration']
+               
+                # Convert ISO 8601 duration to seconds
+                duration_seconds = isodate.parse_duration(duration).total_seconds()
+               
+                # Exclude videos with duration less than 1 minutes (60 seconds)
+               
+                if duration_seconds >= 60:
+                    video_ids.append(video_id)
+
+            next_page_token = response.get('nextPageToken')
 
     return video_ids
 
@@ -45,20 +98,20 @@ def get_video_details(youtube, video_ids):
     views = []
     likes = []
 
+    for i in range(0, len(video_ids), 50):
+        request = youtube.videos().list(
+            part='snippet,statistics',
+            id=','.join(video_ids[i:i+50]))
 
-    request = youtube.videos().list(
-        part='snippet,statistics',
-        id=','.join(video_ids))
+        response = request.execute()
 
-    response = request.execute()
-
-    for video in response['items']:
-        video_urls.append(f"https://www.youtube.com/watch?v={video['id']}"),
-        titles.append(video['snippet']['title']),
-        date = datetime.strptime(str(video['snippet']['publishedAt']), '%Y-%m-%dT%H:%M:%SZ')
-        dates.append(date.date()),
-        views.append(video['statistics']['viewCount']),
-        likes.append(video['statistics']['likeCount'])
+        for video in response['items']:
+            video_urls.append(f"https://www.youtube.com/watch?v={video['id']}"),
+            titles.append(video['snippet']['title']),
+            date = datetime.strptime(str(video['snippet']['publishedAt']), '%Y-%m-%dT%H:%M:%SZ')
+            dates.append(date.date()),
+            views.append(video['statistics']['viewCount']),
+            likes.append(video['statistics']['likeCount'])
         
 
     return video_urls, titles, dates, views, likes
@@ -82,10 +135,13 @@ if __name__ == "__main__":
     api_key = os.getenv('YT_API_KEY')
 
     # change this id according to the channel you want to scrape on every run.
-    channel_id = "UCNU_lfiiWBdtULKOw6X0Dig" #krish naik channel id
+    channel_id = "UCYSbIYzulx3HT5_OQLUH38A" #Jason M. Lemkin channel id
+
+    # channel_id = "UCNU_lfiiWBdtULKOw6X0Dig" #krish naik id
+
 
     # modify the channel_name as per your choice
-    channel_name = 'KrishNaik'
+    channel_name = 'JasonLemkin'
 
     youtube = build(serviceName='youtube', version='v3', developerKey=api_key)
     id = get_channel_id(youtube=youtube, channel_id=channel_id)
